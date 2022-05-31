58352adf4080162f61a92bc283778c8e75ebd9e1
olwyn hogan <hoganol@tcd.ie>
2022-05-31 22:06:16 +0200

    did show page- need to finish html & css

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts, only: %i[new create]
+  resources :posts, only: %i[new create show]
   resources :profiles, only: %i[new create]
-
 end
