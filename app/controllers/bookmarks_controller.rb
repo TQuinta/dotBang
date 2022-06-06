ad7c7e8fd2e73b6dabec8fdc24b95d1de96122e1
Tomas Quintanilha <tquinta@alumni.ie.edu>
2022-06-06 17:50:25 +0200

    bookmark buttons on post show and bookmark index done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,20 @@
 class BookmarksController < ApplicationController
   def index
-    @my_bookmarks = Bookmark.where(user_id: current_user.id)
+    @user_bookmarks = Bookmark.where(user: current_user)
+  end
+
+  def create
+    @bookmark = Bookmark.new
+    @bookmark.user = current_user
+    @bookmark.post = Post.find(params[:post_id])
+    redirect_to post_path(@bookmark.post)
+    flash[:alert] = 'Please try again!' unless @bookmark.save
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_back(fallback_location: post_path(@bookmark.post))
+    @bookmark = nil
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 
   def show
     @author = Profile.find_by(user_id: @post.user_id)
+    @bookmark = @post.bookmarks.find_by(user: current_user) || Bookmark.new
   end
 
   def new
@@ -33,33 +34,10 @@ class PostsController < ApplicationController
     end
   end
 
-  def upvote
-    handle_vote
-    respond_to do |format|
-      format.html { redirect_to post_path(@post) }
-      format.json do
-        render json: {
-          votes: helpers.pluralize(@post.votes.count, "vote"),
-          button_html: render_to_string(partial: "posts/upvote", locals: { post: @post }, formats: [:html])
-        }
-      end
-    end
-  end
-
   private
 
   def set_vote
     @vote = @post.votes.find_by(user: current_user)
-  end
-
-  def handle_vote
-    if author?
-      flash[:alert] = 'Cannot vote on your own post!'
-    elsif @vote
-      remove_votes
-    else
-      add_votes
-    end
   end
 
   def role?
@@ -82,20 +60,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :blurb, :rich_content, photos: [])
   end
 
-  def author?
-    current_user == @post.user
-  end
-
   def set_params
     @post = Post.find(params[:id])
-  end
-
-  def remove_votes
-    @vote.destroy
-    @vote = nil
-  end
-
-  def add_votes
-    @vote = Vote.create(user: current_user, post: @post)
   end
 end
