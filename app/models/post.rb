b93d8676bcd7d9b9dbf5e600862be5e8e088d551
Tomas Quintanilha <tquinta@alumni.ie.edu>
2022-06-06 10:17:15 +0200

    active_text has been incorporated

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :postable, polymorphic: true
   # belongs_to :profile, through: :users
   has_many :votes
-
+  has_rich_text :content
   validates :title, :blurb, :content, presence: true
 
   CATEGORIES = ["Roles", "Skills"]
