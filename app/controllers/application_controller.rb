27034dd2e29c768097077e36f8a380d11c24a40c
Jaqueline Maldonado <jmaldonado262@outlook.com>
2022-06-09 14:45:04 +0200

    signup fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :retrieve_last_index_page_or_default
 
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
 
   def retrieve_last_index_page_or_default(default_path: root_path)
     session[:last_index_page] || default_path
+  end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
   end
 end
